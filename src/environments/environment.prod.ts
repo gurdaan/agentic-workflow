3d705d447c778cf7980ef3640a87bb7476450f46
Gurdaan Walia <gurdaan.walia@jonasconstruction.com>
2025-09-18 22:19:31 -0400

    Update To Prod URL

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   apiConfig: {
-    baseUrl: 'https://jonas.internal.victorioussmoke-3dca34aa.westus2.azurecontainerapps.io',
+    baseUrl: 'https://jonas.victorioussmoke-3dca34aa.westus2.azurecontainerapps.io',
     endpoints: {
       chat: '/api/chat',
       chatSessions: '/api/chat-sessions',
